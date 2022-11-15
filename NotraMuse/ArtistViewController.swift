3eb1736b25371543231ff962e5490ffdadf09ed9
NPlanche <89564236+NPlanche@users.noreply.github.com>
2022-11-15 16:28:46 -0500

    view controller

---
+++ NotraMuse/ArtistViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ArtistViewController.swift
+//  NotraMuse
+//
+//  Created by user204225 on 11/15/22.
+//
+
+import UIKit
+
+class ArtistViewController: UIViewController {
+
+    
+    @IBOutlet weak var artistLabel: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var numfansLabel: UILabel!
+    
+    
+    @IBOutlet weak var numalbumLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NotraMuse/TrackViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TrackViewController.swift
+//  NotraMuse
+//
+//  Created by user204225 on 11/15/22.
+//
+
+import UIKit
+
+class TrackViewController: UIViewController {
+    
+    
+    @IBOutlet weak var trackimg: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabl: UILabel!
+    
+    
+    @IBOutlet weak var rankLabel: UILabel!
+    
+    
+    @IBOutlet weak var artistnameLabel: UILabel!
+    
+    
+    @IBOutlet weak var releaseLabel: UILabel!
+    
+    
+    @IBOutlet weak var tracknumLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
